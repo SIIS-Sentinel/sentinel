60482c74470979774e75e8a0ab122bf3b585083c
AdrienCos <ad.cosson@gmail.com>
2019-06-06 16:01:45 -0400

    Version 1.0 of Sentinel

--- sentinel.c
+++ sentinel.c
@@ -19,20 +19,36 @@
 #include <linux/sysinfo.h>
 
 #define procfs_name "sentinel"
-#define MESSAGE "Welcome to Sentinel\0"
+#define MESSAGE "Welcome to Sentinel\n"
 #define BUF_SIZE 64
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Adrien Cosson");
 MODULE_DESCRIPTION("Hardware monitoring module");
-MODULE_VERSION("0.3");
+MODULE_VERSION("1.0");
 
 static int __initdata data = 3;
 struct proc_dir_entry* proc_file;
 static char message[BUF_SIZE];
 static char* msg_ptr;
 static long int msg_len;
-static int max_iterations = 10;
+
+// Called by procfile_read() to copy some data into the user buffer
+// Args:
+//  user_buf: destination buffer
+//  text: message to prepend to the data
+//  offset: offset to use when writing in the buffer
+//  data: data to write in the buffer
+// Return value:
+//  new offset
+int copy_data(char* __user user_buf, const char* text, int offset, unsigned long data)
+{
+    char data_buf[BUF_SIZE];
+    sprintf(data_buf, "%s%lu kB\n", text, data << (PAGE_SHIFT - 10));
+    copy_to_user(user_buf + offset, data_buf, strlen(data_buf));
+    offset += strlen(data_buf);
+    return offset;
+}
 
 // Called when /proc/sentinel is read
 // Args:
@@ -45,24 +61,39 @@ static int max_iterations = 10;
 ssize_t procfile_read(struct file* file, char __user* buffer, size_t len, loff_t* offset)
 {
     int bytes_copied = 0;
-    char counter_char[BUF_SIZE];
+    struct sysinfo info;
+    void (*si_swapinfo)(struct sysinfo*);
     printk(KERN_INFO "Read handler of Sentinel called\n");
     printk(KERN_INFO "Length: %ld, offset: %lld\n", len, *offset);
 
-    // Verify if we have sent the message enough times
-    if (*offset == max_iterations) {
+    // Gather the data
+    si_swapinfo = (void (*)(struct sysinfo*))kallsyms_lookup_name("si_swapinfo");
+    si_meminfo(&info);
+    si_swapinfo(&info);
+
+    switch (*offset) {
+    case 0:
+        // Copy the message
+        copy_to_user(buffer + bytes_copied, message, msg_len);
+        bytes_copied += msg_len;
+        break;
+    case 1:
+        // Copy the total memory
+        bytes_copied = copy_data(buffer, "Total memory:\t", bytes_copied, info.totalram);
+        break;
+    case 2:
+        // Copy the free memory
+        bytes_copied = copy_data(buffer, "Free memory:\t", bytes_copied, info.freeram);
+        break;
+    case 3:
+        // Copy the total memory
+        bytes_copied = copy_data(buffer, "Used memory:\t", bytes_copied, info.totalram - info.freeram);
+        break;
+    default:
+        // We are done with reading
         return 0;
+        break;
     }
-
-    // Copy the message
-    copy_to_user(buffer, message, msg_len);
-    bytes_copied += msg_len;
-
-    // Copy the counter
-    printk(KERN_INFO "Copying offset with value %lld\n", *offset);
-    sprintf(counter_char, ", offset: %lld\n", *offset);
-    copy_to_user(buffer + bytes_copied, counter_char, strlen(counter_char));
-    bytes_copied += strlen(counter_char);
     (*offset)++;
 
     printk(KERN_INFO "Number of bytes copied: %d", bytes_copied);
@@ -77,13 +108,22 @@ ssize_t procfile_read(struct file* file, char __user* buffer, size_t len, loff_t
 //  offset: offset where the last write stopped
 // Return value:
 //  number of bytes copied from the buffer. The function keeps being called until out == len
+// Notes:
+//  when the function is called multiple times on the same data, the buffer is changed to remove the written data
 ssize_t procfile_write(struct file* file, const char __user* buffer, size_t len, loff_t* offset)
 {
+    char kbuf[BUF_SIZE];
     printk(KERN_INFO "Write handler of Sentinel called\n");
     printk(KERN_INFO "Length: %ld, offset: %lld\n", len, *offset);
-    // printk(KERN_ALERT "This device does not support write operations\n");
+    // Make sure we do not overflow the buffer
+    if (len > BUF_SIZE) {
+        printk(KERN_INFO "Truncating to buffer size\n");
+        len = BUF_SIZE;
+    }
+    copy_from_user(kbuf, buffer, len);
+    printk(KERN_INFO "Message: %.*s", (int)len, kbuf);
     (*offset)++;
-    return 1;
+    return len;
 }
 
 int output_memory_use(void)
