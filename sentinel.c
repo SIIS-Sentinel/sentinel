f7e7dbe3dd1cdf2cdcdab2c92a2dec934f1878ca
AdrienCos <ad.cosson@gmail.com>
2019-06-14 13:46:48 -0400

    Nested the proc file in a folder

--- sentinel.c
+++ sentinel.c
@@ -21,14 +21,18 @@
 #include <linux/swap.h>
 #include <linux/sysinfo.h>
 
-#define procfs_name "sentinel"
+#define procfs_dir_name "sentinel"
+#define procfs_name_single "single"
+#define procfs_name_iter "iter"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Adrien Cosson");
 MODULE_DESCRIPTION("Hardware monitoring module");
 MODULE_VERSION("1.0");
 
-struct proc_dir_entry* proc_file;
+struct proc_dir_entry* proc_dir;
+struct proc_dir_entry* proc_file_single;
+struct proc_dir_entry* proc_file_iter;
 
 static void show_val_kb(struct seq_file* seq, const char* s, unsigned long num)
 {
@@ -101,13 +105,17 @@ static struct file_operations f_ops = {
 static int __init hello(void)
 {
     printk(KERN_INFO "Hello world\n");
-    proc_file = proc_create(procfs_name, 0444, NULL, &f_ops);
+    proc_dir = proc_mkdir(procfs_dir_name, NULL);
+    proc_file_single = proc_create(procfs_name_single, 0444, proc_dir, &f_ops);
+    proc_file_iter = proc_create(procfs_name_iter, 0444, proc_dir, &f_ops);
     return 0;
 }
 
 static void __exit goodbye(void)
 {
-    proc_remove(proc_file);
+    proc_remove(proc_file_single);
+    proc_remove(proc_file_iter);
+    proc_remove(proc_dir);
     printk(KERN_INFO "Goodbye world\n");
 }
 
